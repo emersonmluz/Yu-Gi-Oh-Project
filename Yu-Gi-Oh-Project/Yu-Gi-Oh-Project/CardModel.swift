3ad4011aaf1b498bba672ca28edef03e5c80804b
Emerson M Luz <emerson.maciel@vortigo.com.br>
2023-02-22 12:20:22 -0300

    Adicionado mais var no modelo de card

--- Yu-Gi-Oh-Project/Yu-Gi-Oh-Project/CardModel.swift
+++ Yu-Gi-Oh-Project/Yu-Gi-Oh-Project/CardModel.swift
@@ -18,7 +18,9 @@ struct CardModel: Decodable {
     private let level: Int?
     private let style: String
     private let attribute: String?
+    private let sets: [CardSets]
     private let images: [CardImages]
+    private let prices: [CardPrices]
     
     enum CodingKeys: String, CodingKey {
         case id
@@ -31,6 +33,8 @@ struct CardModel: Decodable {
         case level
         case style = "race"
         case attribute
+        case sets = "card_sets"
         case images = "card_images"
+        case prices = "card_prices"
     }
 }
